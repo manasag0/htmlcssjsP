ade8d5aafc843845530ad7be34ab86ba7cd7d8c1
Manasa G <manasa.g7890@hotmail.com>
2023-11-05 16:06:16 +0530

    files modified

--- todo app/script.js
+++ todo app/script.js
@@ -1,11 +1,1 @@
-const button = document.getElementsByTagName('button')
-const newItemText = document.getElementById("todos")
-const list = document.getElementById('todolist')
-const newListItem= document.createElement('li')
-
-//add to do task
-
-function addTodo(newItemText){
-    newListItem.textContent = newItemText;
-    list.appendChild(newListItem);    
-}
+//

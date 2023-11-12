3b453e0a31e539927c04d0d95ad361f27d3b0e88
Manasa G <manasa.g7890@hotmail.com>
2023-11-12 22:08:17 +0530

    api not used

--- videoTrilor popuo/script.js
+++ videoTrilor popuo/script.js
@@ -1,2 +1,19 @@
 const APIKEY = "e0784c265561dc372949c466ee7bc4de";
 const URL = 'https://api.themoviedb.org/3/movie/11?api_key=e0784c265561dc372949c466ee7bc4de';
+
+const btnEl = document.querySelector('.btn');
+const activeEl = document.querySelector('.trailer-container');
+const closeEl = document.querySelector('.close-icon');
+const videoEl = document.getElementsByClassName("video")
+
+
+
+btnEl.addEventListener('click', ()=>{
+    activeEl.classList.remove("active")
+})
+
+closeEl.addEventListener("click",()=>{
+    activeEl.classList.add("active");
+    videoEl.pause();
+    videoEl.currentTime = 0;
+});
